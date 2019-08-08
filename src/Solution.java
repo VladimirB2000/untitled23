b6a678c7af95badb4ec91dd9c0d86a1b23b55445
Vladimir Belkin <vladimirB2000@mail.ru>
2019-08-08 17:03:46 +0700

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int a = 0;
+        int sum = 0;
+
+        while (a != -1) {
+            a = scanner.nextInt();
+            sum = sum + a;
+
+        } System.out.println(sum);
+        }
+    }
+
